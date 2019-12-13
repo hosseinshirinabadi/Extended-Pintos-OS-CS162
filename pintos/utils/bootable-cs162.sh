653b2ec1042220988c77b5a63fa001039c87ebe5
Sam Kumar <samkumar99@gmail.com>
2019-12-12 23:34:49 -0800

    Pintos Fun: Update bootable-cs162.sh to work better with simple demo

--- pintos/utils/bootable-cs162.sh
+++ pintos/utils/bootable-cs162.sh
@@ -7,17 +7,34 @@ OUTPUT=cs162proj.dsk
 LOADER=$BUILD/loader.bin
 KERNEL=$BUILD/kernel.bin
 
-ARGS="--kernel=$KERNEL --loader $LOADER"
-echo $ARGS
-
 make -C $EXAMPLES
 
 rm -f $OUTPUT
 pintos -v -k --make-disk $OUTPUT --kernel=$KERNEL --loader $LOADER --qemu --filesys-size=4 --align=full -- -q  -f
 
 PREFIX=
-if [ "$1" != "--simple" ]
+if [ "$1" == "--simple" ]
 then
+	# If someone has really only done Project 1, then their root directory
+	# can contain at most 16 entries. So we have to use them wisely. Two
+	# spaces are used by file1.txt and file2.txt. I'm reserving two more for
+	# one music demo and the contest, should they choose to do it. That leaves
+	# 12 spaces for binaries. I'm including ls, mkdir, and pwd so students can
+	# experience that aspect of it if they have their Project 3 and choose to
+	# start with the simple shell.
+	rm -f $EXAMPLES/auld
+	rm -f $EXAMPLES/bubsort
+	rm -f $EXAMPLES/fappend
+	rm -f $EXAMPLES/fcreate
+	rm -f $EXAMPLES/lineup
+	rm -f $EXAMPLES/matmult
+	rm -f $EXAMPLES/mcat
+	rm -f $EXAMPLES/mcp
+	rm -f $EXAMPLES/railroad
+	rm -f $EXAMPLES/recursor
+	rm -f $EXAMPLES/rmrec
+	rm -f $EXAMPLES/yankee
+else
 	PREFIX=/bin/
 	pintos -v -k --qemu --disk $OUTPUT -p $EXAMPLES/mkdir -a mkdir -- -q run "mkdir bin"
 	pintos -v -k --qemu --disk $OUTPUT -p $EXAMPLES/rm -a rm -- -q run "rm mkdir rm"
