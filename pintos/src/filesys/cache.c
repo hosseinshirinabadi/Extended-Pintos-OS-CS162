6f2dfc8230e26650ceaa7b885538f0dc192fff76
Pedram Pourdavood <pedrampd@berkeley.edu>
2020-04-24 22:51:43 +0000

    start fully assoc

--- pintos/src/filesys/cache.c
+++ pintos/src/filesys/cache.c
@@ -18,6 +18,7 @@ struct cache_entry {
 struct cache_entry cache[64];
 struct list LRU;
 struct lock LRU_lock;
+int count = 0;
 
 
 void initialize_cache() {
@@ -34,24 +35,64 @@ void initialize_cache() {
 
 void read_from_cache(block_sector_t target_sector, void *buff) {
   assert(sizeof(buff) == BLOCK_SECTOR_SIZE);
-  struct cache_entry *block = &cache[target_sector % 64];
-  if (block->sector == target_sector) {
-      lock_acquire(&block->cache_lock);
+  lock_acquire(&LRU_lock);
+
+  struct cache_entry *block;
+  for (int i = 0; i < access_count; i++) {
+    block = &cache[i];
+    if (block->sector == target_sector) {
       memcpy(buff, block->data, BLOCK_SECTOR_SIZE);
-      lock_release (&block->cache_lock);
+      lock_release(&lock_release);
 
       update_LRU(block);
       return;
-  } else {
+    }
+  }
+
+  if (access_count < 64) {
+    // find an empty block
+    block = &cache[access_count];
+    access_count++;
+
     lock_acquire(&block->cache_lock);
     block->sector = target_sector;
+
     block_read(fs_device, target_sector, buff);
     memcpy(block->data, buff, BLOCK_SECTOR_SIZE);
+
     block->dirty = false;
+    block->valid = true;
+    block->num_readers++;
     lock_release(&block->cache_lock);
 
     update_LRU(block);
+  } else {
+    // replace
+
   }
+
+
+
+
+
+  // struct cache_entry *block = &cache[target_sector % 64];
+  // if (block->sector == target_sector) {
+  //     lock_acquire(&block->cache_lock);
+  //     memcpy(buff, block->data, BLOCK_SECTOR_SIZE);
+  //     lock_release (&block->cache_lock);
+
+  //     update_LRU(block);
+  //     return;
+  // } else {
+  //   lock_acquire(&block->cache_lock);
+  //   block->sector = target_sector;
+  //   block_read(fs_device, target_sector, buff);
+  //   memcpy(block->data, buff, BLOCK_SECTOR_SIZE);
+  //   block->dirty = false;
+  //   lock_release(&block->cache_lock);
+
+  //   update_LRU(block);
+  // }
 
     
 }
@@ -61,7 +102,13 @@ void write_to_cache(block_sector_t target_sector, void *buff) {
 
 }
 
-void update_LRU(struct cache_entry *block) {
+void update_LRU1(struct cache_entry *block) {
+  lock_acquire(&LRU_lock);
+  list_push_back(&LRU, &block->elem);
+  lock_release(&LRU_lock);
+}
+
+void update_LRU2(struct cache_entry *block) {
   lock_acquire(&LRU_lock);
   list_remove(&block->elem);
   list_push_back(&LRU, &block->elem);
