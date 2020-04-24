83b85e5f709e94002688cf4ba9f2b705fbd6ac95
Pedram Pourdavood <pedrampd@berkeley.edu>
2020-04-24 21:30:13 +0000

    start direct mapped

--- pintos/src/filesys/cache.c
+++ pintos/src/filesys/cache.c
@@ -4,10 +4,10 @@
 #include <stdbool.h>
 #include <list.h>
 
-struct cache_block {
+struct cache_entry {
   block_sector_t sector;  // sector number on disk
   char data[BLOCK_SECTOR_SIZE];  // cached data
-  bool dirty;  // block was written or not 
+  bool dirty;  // block was written or not
   bool valid;
   struct lock cache_lock;  // lock for this cache
   int num_readers;
@@ -15,12 +15,12 @@ struct cache_block {
   struct list_elem elem;  // list_elem used for the LRU list
 };
 
-struct cache_block cache[64];
+struct cache_entry cache[64];
 struct list LRU;
 struct lock LRU_lock;
 
 
-void init_cache() { 
+void initialize_cache() {
   // initialize cache locks
   for (int i = 0; i < 64; i++) {
     lock_init(&(cache[i].cache_lock));
@@ -33,20 +33,24 @@ void init_cache() {
 
 
 void read_from_cache(block_sector_t target_sector, void *buff) {
-  struct cache_block *block = &cache[target_sector % 64];
+  assert(sizeof(buff) == BLOCK_SECTOR_SIZE);
+  struct cache_entry *block = &cache[target_sector % 64];
   if (block->sector == target_sector) {
       lock_acquire(&block->cache_lock);
       memcpy(buff, block->data, BLOCK_SECTOR_SIZE);
       lock_release (&block->cache_lock);
 
-      lock_acquire(&LRU_lock);
-      list_remove(&block->elem);
-      list_push_back(&LRU, &block->elem);
-      lock_release(&LRU_lock);
+      update_LRU(block);
       return;
   } else {
+    lock_acquire(&block->cache_lock);
     block->sector = target_sector;
-    
+    block_read(fs_device, target_sector, buff);
+    memcpy(block->data, buff, BLOCK_SECTOR_SIZE);
+    block->dirty = false;
+    lock_release(&block->cache_lock);
+
+    update_LRU(block);
   }
 
     
@@ -55,6 +59,13 @@ void read_from_cache(block_sector_t target_sector, void *buff) {
 
 void write_to_cache(block_sector_t target_sector, void *buff) {
 
+}
+
+void update_LRU(struct cache_entry *block) {
+  lock_acquire(&LRU_lock);
+  list_remove(&block->elem);
+  list_push_back(&LRU, &block->elem);
+  lock_release(&LRU_lock);
 }
 
 
