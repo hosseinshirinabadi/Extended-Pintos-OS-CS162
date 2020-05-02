0fc41dcfcaa1196c2044cac0d8d4044d495257a3
sinahdalir <sdalir@berkeley.edu>
2020-05-02 07:33:30 +0000

    shutdown fix

--- pintos/src/filesys/cache.h
+++ pintos/src/filesys/cache.h
@@ -9,5 +9,6 @@ struct cache_block;
 void initialize_cache(void);
 void read_from_cache(block_sector_t target_sector, void *buff);
 void write_to_cache(block_sector_t target_sector, void *buff);
+void flush_cache(void);
 
 #endif /* filesys/cache.h */
