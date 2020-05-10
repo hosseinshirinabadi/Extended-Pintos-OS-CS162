3d2e053c49fc89fff02d7d0202300cad0226e2f0
Hossein <hossein_shirinabadi@berkeley.edu>
2020-05-10 20:04:51 +0000

    last test1 version

--- pintos/src/tests/filesys/extended/CacheTest1.c
+++ pintos/src/tests/filesys/extended/CacheTest1.c
@@ -4,70 +4,71 @@
 #include "tests/main.h"
 #include "filesys/cache.h"
 
-#define NUM_CACHE_ENTRIES 64
+#define NUM_ENTRIES 30
 static char tempBuf[BLOCK_SECTOR_SIZE];
 
 void
 test_main (void)
 {
-  int file;
-  size_t ret_val;
-  random_init (0);
-  random_bytes (tempBuf, sizeof tempBuf);
-  CHECK (create ("a", 0), "create \"a\"");
-  CHECK ((file = open ("a")) > 1, "open \"a\"");
 
-  
-  for(int i=0; i < NUM_CACHE_ENTRIES; i++)
-  {
-    ret_val = write (file, tempBuf, BLOCK_SECTOR_SIZE);
-    if (ret_val != BLOCK_SECTOR_SIZE)
-      fail ("write %zu bytes in \"a\" returned %zu",
-            BLOCK_SECTOR_SIZE, ret_val);
+  int file;
+  size_t num_bytes;
+
+  /* create a second file and opening it for wrtitin the buffer in it*/
+  CHECK (create ("xyz", 0), "create \"xyz\"");
+  CHECK ((file = open ("xyz")) > 1, "open \"xyz\" for writing");
+
+  /* use a for loop to write random bytes into file*/
+  for(int i=0; i < NUM_ENTRIES; i++) {
+    /* writing random bytes into temp buffer */
+    random_init (0);
+    random_bytes (tempBuf, sizeof tempBuf);
+    num_bytes = write (file, tempBuf, BLOCK_SECTOR_SIZE);
+    if (num_bytes != BLOCK_SECTOR_SIZE)
+      fail("was supposed to write %zu bytes in \"xyz\" but return value is %zu", BLOCK_SECTOR_SIZE, num_bytes);
   }
-  msg ("close \"after writing\"");
+
+  msg ("close \"xyz\" after writing");
   close (file);
 
   msg("cache reset");
-  // buffer_reset ();
+  /* a syscall to reset the cache */
   reset_cache();
 
-  CHECK ((file = open ("a")) > 1, "open \"a\"");
-  msg ("read first time");
-  for (int i = 0; i < NUM_CACHE_ENTRIES; i++)
-  {
-    ret_val = read (file, tempBuf, BLOCK_SECTOR_SIZE);
-    if (ret_val != BLOCK_SECTOR_SIZE)
-        fail ("read %zu bytes in \"a\" returned %zu",
-              BLOCK_SECTOR_SIZE, ret_val);
+  CHECK ((file = open ("xyz")) > 1, "open \"xyz\" for first read");
+  msg ("read for the first time");
+  for (int i = 0; i < NUM_ENTRIES; i++) {
+    num_bytes = read (file, tempBuf, BLOCK_SECTOR_SIZE);
+    if (num_bytes != BLOCK_SECTOR_SIZE)
+      fail ("was supposed to read %zu bytes from \"xyz\" but return value is %zu", BLOCK_SECTOR_SIZE, num_bytes);
   }
-  close (file);
-  msg("close \"a\"");
 
+  msg("close \"xyz\" after first read");
+  close (file);
+  /* calculating the hit rate of first read */
   int first_hit = cache_hit;
   int first_total = cache_hit + cache_miss;
-  int first_hit_rate = (100 * first_hit) / first_total;
 
-  CHECK ((file = open ("a")) > 1, "open \"a\"");
-  msg ("read \"a\"");
-  for (int i = 0; i < NUM_CACHE_ENTRIES; i ++)
-  {
-    ret_val = read (file, tempBuf, BLOCK_SECTOR_SIZE);
-    if (ret_val != BLOCK_SECTOR_SIZE)
-        fail ("read %zu bytes in \"a\" returned %zu",
-              BLOCK_SECTOR_SIZE, ret_val);
+  CHECK ((file = open ("xyz")) > 1, "open \"xyz\" for second read");
+  msg ("read for the second time");
+  for (int i = 0; i < NUM_ENTRIES; i ++) {
+    num_bytes = read (file, tempBuf, BLOCK_SECTOR_SIZE);
+    if (num_bytes != BLOCK_SECTOR_SIZE)
+      fail ("was supposed to read %zu bytes from \"xyz\" but return value is %zu", BLOCK_SECTOR_SIZE, num_bytes);  
   }
+
   close (file);
-  msg("close \"a\"");
+  msg("close \"xyz\" after second read");
 
-  remove ("a");
+  // int second_hit = cache_hit;
+  // int second_total = cache_hit + cache_miss;
 
+  int first_hit_rate = (100 * first_hit) / first_total;
+  int second_hit_rate = (100 * (cache_hit - first_hit)) / (cache_hit + cache_miss - first_total);
 
-  int new_hit = cache_hit ;
-  int new_total = cache_hit + cache_miss;
-  int new_hit_rate = 100 * (new_hit - first_hit) / (new_total - first_total);
+  remove ("xyz");
 
-  if (new_hit_rate>first_hit_rate){
-    msg("Second reading has higher hit rate than the first time");
+  if (second_hit_rate>first_hit_rate){
+    msg("Second read has higher hit rate than the first time");
   }
 }
--- pintos/src/tests/filesys/extended/CacheTest1.ck
+++ pintos/src/tests/filesys/extended/CacheTest1.ck
@@ -4,17 +4,17 @@ use warnings;
 use tests::tests;
 check_expected (IGNORE_USER_FAULTS => 1, [<<'EOF']);
 (CacheTest1) begin
-(CacheTest1) create "a"
-(CacheTest1) open "a"
-(CacheTest1) close "after writing"
+(CacheTest1) create "xyz"
+(CacheTest1) open "xyz"
+(CacheTest1) close after writing
 (CacheTest1) cache reset
-(CacheTest1) open "a"
-(CacheTest1) read first time
-(CacheTest1) close "a"
-(CacheTest1) open "a"
-(CacheTest1) read "a"
-(CacheTest1) close "a"
-(CacheTest1) Second reading has higher hit rate than the first time
+(CacheTest1) open "xyz"
+(CacheTest1) read for the first time
+(CacheTest1) close "xyz" after first read
+(CacheTest1) open "xyz"
+(CacheTest1) read for the second time
+(CacheTest1) close "xyz" after second read
+(CacheTest1) Second read has higher hit rate than the first time
 (CacheTest1) end
 CacheTest1: exit(0)
 EOF
